5a8b987a89b65ee15f2099021085ab3c6a9ec0b0
dsc <dsc@less.ly>
2013-03-25 14:25:17 -0700

    Adds Hadoop Job Conf file, queue name, and job name parameters.

--- src/main/java/kafka/consumer/HadoopConsumer.java
+++ src/main/java/kafka/consumer/HadoopConsumer.java
@@ -1,19 +1,17 @@
 package kafka.consumer;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.PosixParser;
+import org.apache.commons.cli.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.util.Tool;
@@ -25,6 +23,11 @@ public class HadoopConsumer extends Configured implements Tool {
         Configuration.addDefaultResource("core-site.xml");
         //Configuration.addDefaultResource("mapred-site.xml");
     }
+
+    private Options options;
+    private CommandLine cmd;
+
+
     
     public static class KafkaMapper extends Mapper<LongWritable, BytesWritable, LongWritable, Text> {
         @Override
@@ -40,7 +43,9 @@ public class HadoopConsumer extends Configured implements Tool {
         }
         
     }
-    
+
+
+    @Override
     public int run(String[] args) throws Exception {
         
         //ToolRunner.printGenericCommandUsage(System.err);
@@ -51,26 +56,35 @@ public class HadoopConsumer extends Configured implements Tool {
         }
         */
 
-        CommandLineParser parser = new PosixParser();
-        Options options = buildOptions();
-        
-        CommandLine cmd = parser.parse(options, args);
-        
+        cmd = getOptions(args);
         //HelpFormatter formatter = new HelpFormatter();
-        //formatter.printHelp( "kafka.consumer.hadoop", options );
-        
+        //formatter.printHelp("kafka.consumer.hadoop", options);
+
         Configuration conf = getConf();
-        conf.set("kafka.topic", cmd.getOptionValue("topic", "test"));
-        conf.set("kafka.groupid", cmd.getOptionValue("consumer-group", "test_group"));
+
+        String confPath = cmd.getOptionValue("job-conf");
+        if (confPath != null)
+            conf.addResource(new FileInputStream(confPath), confPath);
+
+        // TODO: Allow conf file to set these properties
+        String kafkaTopic = cmd.getOptionValue("topic", "test");
+        String kafkaGroup = cmd.getOptionValue("consumer-group", "test_group");
+        conf.set("kafka.topic", kafkaTopic);
+        conf.set("kafka.groupid", kafkaGroup);
         conf.set("kafka.zk.connect", cmd.getOptionValue("zk-connect", "localhost:2182"));
         if (cmd.getOptionValue("autooffset-reset") != null)
             conf.set("kafka.autooffset.reset", cmd.getOptionValue("autooffset-reset"));
         conf.setInt("kafka.limit", Integer.valueOf(cmd.getOptionValue("limit", "-1")));
-        
+
         conf.setBoolean("mapred.map.tasks.speculative.execution", false);
-        
-        
-        Job job = new Job(conf, "Kafka.Consumer");
+        String jobQueue = cmd.getOptionValue("queue");
+        if (jobQueue != null) {
+            conf.set("mapred.job.queue.name",   jobQueue);
+            conf.set("mapreduce.job.queuename", jobQueue);
+        }
+
+        String jobName = cmd.getOptionValue("name", "Kafka.Consumer(topic="+kafkaTopic+", group="+kafkaGroup+")");
+        Job job = Job.getInstance(conf, jobName);
         job.setJarByClass(getClass());
         job.setMapperClass(KafkaMapper.class);
         // input
@@ -94,9 +108,9 @@ public class HadoopConsumer extends Configured implements Tool {
     private void commit(Configuration conf) throws IOException {
         ZkUtils zk = new ZkUtils(conf);
         try {
-            String topic = conf.get("kafka.topic");
-            String group = conf.get("kafka.groupid");
-            zk.commit(group, topic);
+            String kafkaTopic = conf.get("kafka.topic");
+            String kafkaGroup = conf.get("kafka.groupid");
+            zk.commit(kafkaGroup, kafkaTopic);
         } catch (Exception e) {
             rollback();
         } finally {
@@ -107,42 +121,70 @@ public class HadoopConsumer extends Configured implements Tool {
     private void rollback() {
     }
 
+    private CommandLine getOptions(String[] args) throws ParseException {
+        if (cmd != null) return cmd;
+
+        CommandLineParser parser = new PosixParser();
+        cmd = parser.parse(buildCLIOptions(), args);
+        return cmd;
+    }
+
     @SuppressWarnings("static-access")
-    private Options buildOptions() {
+    private static Options buildCLIOptions() {
         Options options = new Options();
         
-        options.addOption(OptionBuilder.withArgName("topic")
-                            .withLongOpt("topic")
-                            .hasArg()
-                            .withDescription("kafka topic")
-                            .create("t"));
-        options.addOption(OptionBuilder.withArgName("groupid")
+        options.addOption(
+            OptionBuilder.withArgName("topic")
+                .withLongOpt("topic")
+                .hasArg()
+                .withDescription("Kafka topic")
+                .create("t"));
+        options.addOption(
+            OptionBuilder.withArgName("groupid")
                 .withLongOpt("consumer-group")
                 .hasArg()
-                .withDescription("kafka consumer groupid")
+                .withDescription("Kafka consumer groupid")
                 .create("g"));
-        options.addOption(OptionBuilder.withArgName("zk")
+        options.addOption(
+            OptionBuilder.withArgName("zk")
                 .withLongOpt("zk-connect")
                 .hasArg()
-                .withDescription("ZooKeeper connection String")
+                .withDescription("ZooKeeper connection string")
                 .create("z"));
-        
-        options.addOption(OptionBuilder.withArgName("offset")
+        options.addOption(
+            OptionBuilder.withArgName("offset")
                 .withLongOpt("autooffset-reset")
                 .hasArg()
-                .withDescription("Offset reset")
+                .withDescription("Kafka offset reset")
                 .create("o"));
-        
-        options.addOption(OptionBuilder.withArgName("limit")
+        options.addOption(
+            OptionBuilder.withArgName("limit")
                 .withLongOpt("limit")
                 .hasArg()
-                .withDescription("kafka limit")
+                .withDescription("Kafka limit")
                 .create("l"));
-        
+        options.addOption(
+            OptionBuilder.withArgName("name")
+                .withLongOpt("name")
+                .hasArg()
+                .withDescription("Hadoop job name")
+                .create("n"));
+        options.addOption(
+            OptionBuilder.withArgName("queue")
+                .withLongOpt("queue")
+                .hasArg()
+                .withDescription("Hadoop job queue name")
+                .create("q"));
+        options.addOption(
+            OptionBuilder.withArgName("conf")
+                .withLongOpt("job-conf")
+                .hasArg()
+                .withDescription("Hadoop job configuration file")
+                .create("C"));
 
         return options;
     }
-    
+
     public static void main(String[] args) throws Exception {
         int exitCode = ToolRunner.run(new HadoopConsumer(), args);
         System.exit(exitCode);
